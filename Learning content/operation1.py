8671132563bb2ccd4a6899beb9008677350cdddf
Kim Hyo Chan <skkhc0317@g.yju.ac.kr>
2024-04-29 14:21:22 +0900

    4월 29일 업데이트

---
+++ Learning content/operation1.py
@@ -0,0 +1,9 @@
+# 정수와 부동 소수점 수 연산
+num_int = 10  # "="은 이항 연산자
+num_float = 3.14
+
+# 연산 수행
+result = num_int + num_float  # 정수와 부동 소수점 수의 합
+
+print("결과: ", result)   # 출력: 결과: 13.14
+print("결과의 자료형: ")   # 출력: 결과의 자료형: <class 'float'>
