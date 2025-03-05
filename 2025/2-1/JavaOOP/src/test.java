2a73043b3584e5d03b92a9f1b0b804f61ba5d16e
Kim Hyo Chan <skkhc0317@gmail.com>
2025-03-05 19:52:13 +0900

    25.03.05. study OOP & clean files

---
+++ 2025/2-1/JavaOOP/src/test.java
@@ -0,0 +1,20 @@
+//1. Class 정의 -> 틀 정의
+// 틀을 정의하는 이유 : 객체를 찍어내기 위해
+// Class의 이름을 정의할 때 첫 글자는 대문자로 함
+class Car{
+    // Data + Function
+    // Data -> Member variable(멤버 변수)
+    // 멤버 변수란 클래스 안에 속해 있는 변수 라는 것
+    String name;
+
+    // Function -> Member method(멤버 메서드)
+    void prtName() {
+        System.out.println(name);
+    }
+}
+
+public class test {
+    public static void main(String[] args) {
+
+    }
+}
