7503a13129ffb5da25649465bb0ba440b0676377
Kim Hyo Chan <skkhc0317@gmail.com>
2025-03-21 14:26:50 +0900

    25.03.21. Update OOP study

---
+++ 2025/2-1/JavaOOP/src/learningContents/test/test.java
@@ -0,0 +1,18 @@
+package learningContents.test;
+
+class Bar {
+    /*
+    Java에서의 클래스 구성 요소
+    1. 멤버 변수
+    2. 멤버 메서드
+    3. 생성자
+    4. 초기화 블록
+    5. 소멸자
+     */
+}
+
+public class test {
+    public static void main(String[] args) {
+
+    }
+}
